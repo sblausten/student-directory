f4106048b61eb1c1220e9fd446e5c53e8b0e0276
sblausten <sblausten@gmail.com>
2017-03-30 13:13:23 +0100

    Refactored by splitting into methods

--- directory.rb
+++ directory.rb
@@ -1,18 +1,31 @@
 
 student_names = [
-	"Dr. Hannibal Lecter",
-	"Darth Vader",
-	"Nurse Ratched",
-	"Michael Corleone",
-	"Alex DeLarge",
-	"The Wicked Witch of the West",
-	"Terminator",
-	"Freddy Krueger",
-	"The Joker",
-	"Joffrey Baratheon",
-	"Norman Bates"]
-student_count = student_names.count
-puts "The students of Villains Academy"
-puts "-------"
-puts student_names[0..student_count]
-puts "Overall we have #{student_count} great students."
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+"Norman Bates"]
+
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------"
+end
+
+def print_names(array)
+  array.each {|name| puts name}
+end
+
+def print_footer(array)
+  puts "Overall we have #{array.count} great students."
+end
+
+print_header
+print_names(students)
+print_footer(students)
