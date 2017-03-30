f914703ca0efd2df94108c41b8fa8cbb2a76976e
sblausten <sblausten@gmail.com>
2017-03-30 15:14:44 +0100

    User inputs list of students themselves.

--- directory.rb
+++ directory.rb
@@ -1,18 +1,16 @@
 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: "April"},
-  {name: "Darth Vader", cohort: "April"},
-  {name: "Nurse Ratched", cohort: "April"},
-  {name: "Michael Corleone", cohort: "April"},
-  {name: "Alex DeLarge", cohort: "April"},
-  {name: "The Wicked Witch of the West", cohort: "April"},
-  {name: "Terminator", cohort: "April"},
-  {name: "Freddy Krueger", cohort: "April"},
-  {name: "The Joker", cohort: "April"},
-  {name: "Joffrey Baratheon", cohort: "April"},
-  {name: "Norman Bates", cohort: "April"}
-]
-
+def input_students
+	puts "Please enter names of students. \nAfter each name hit enter. \nTo exit press enter twice."
+	students = []
+	name = gets.chomp
+	while !name.empty? do
+		students << {name: name, cohort: "April"}
+		puts "Added #{students.last[:name]}"
+		puts "There are now #{students.count} students."
+		name = gets.chomp
+	end
+	students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -27,6 +25,25 @@ def print_footer(array)
   puts "Overall we have #{array.count} great students."
 end
 
+students = input_students
 print_header
 print_names(students)
 print_footer(students)
+
+
+=begin
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: "April"},
+  {name: "Darth Vader", cohort: "April"},
+  {name: "Nurse Ratched", cohort: "April"},
+  {name: "Michael Corleone", cohort: "April"},
+  {name: "Alex DeLarge", cohort: "April"},
+  {name: "The Wicked Witch of the West", cohort: "April"},
+  {name: "Terminator", cohort: "April"},
+  {name: "Freddy Krueger", cohort: "April"},
+  {name: "The Joker", cohort: "April"},
+  {name: "Joffrey Baratheon", cohort: "April"},
+  {name: "Norman Bates", cohort: "April"}
+]
+
+=end
