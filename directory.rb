ca571307405a7c5d75efbea89443adb05455c16f
sblausten <sblausten@gmail.com>
2017-03-30 13:04:20 +0100

    reduced repetition and coupling

--- directory.rb
+++ directory.rb
@@ -1,15 +1,18 @@
-student_count = 11
+
+student_names = [
+	"Dr. Hannibal Lecter",
+	"Darth Vader",
+	"Nurse Ratched",
+	"Michael Corleone",
+	"Alex DeLarge",
+	"The Wicked Witch of the West",
+	"Terminator",
+	"Freddy Krueger",
+	"The Joker",
+	"Joffrey Baratheon",
+	"Norman Bates"]
+student_count = student_names.count
 puts "The students of Villains Academy"
 puts "-------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts student_names[0..student_count]
 puts "Overall we have #{student_count} great students."
