527a2bcbea7acc8a696e5dc17eb9d46c138c03cf
sblausten <sblausten@gmail.com>
2017-04-01 17:05:06 +0100

    Revert "Step 10 - finished"
    This reverts commit ba96573829d09d6328228a215a7d698441d4f446.

--- step10.rb
+++ step10.rb
@@ -4,39 +4,42 @@ def input_students
   puts "Please enter names of students. \nAfter each name hit enter. \nTo exit press enter twice."
   name = gets.chomp
   while !name.empty? do
-      @students << {name: name, cohort: "April"}
-      puts "Added #{@students.last[:name]}"
-      puts "There are now #{@students.count} students."
-      name = gets.chomp
-    end
+    @students << {name: name, cohort: "April"}
+    puts "Added #{@students.last[:name]}"
+    puts "There are now #{@students.count} students."
+    name = gets.chomp
   end
+end
 
-  def print_header
-    puts "The students of Villains Academy"
-    puts "-------"
-  end
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------"
+end
 
-  def print_names
-    @students.each {|student| puts "#{student[:name]}, #{student[:cohort]} cohort"}
-  end
+def print_names
+  @students.each {|student| puts "#{student[:name]}, #{student[:cohort]} cohort"}
+end
 
-  def print_footer
-    puts "Overall we have #{@students.count} great students."
-  end
+def print_footer
+  puts "Overall we have #{@students.count} great students."
+end
 
-  def print_menu
-    p "1. Input the students"
-    p "2. Show the students"
-    p "9. Exit"
-  end
+def print_menu
+  p "1. Input the students"
+  p "2. Show the students"
+  p "9. Exit"
+end
 
-  def show_students
-    print_header
-    print_names
-    print_footer
-  end
+def show_students
+  print_header
+  print_names
+  print_footer
+end
 
-  def process(selection)
+def interactive_menu
+  loop do 
+    print_menu
+    selection = gets.chomp
     case selection
     when "1"
       input_students
@@ -44,16 +47,10 @@ def input_students
       show_students
     when "9"
       exit
-    else
+    else 
       p "I don't know what you want to do - please enter a number for the command."
     end
   end
+end
 
-  def interactive_menu
-    loop do
-      print_menu
-      process(gets.chomp)
-    end
-  end
-
-  interactive_menu
+interactive_menu
